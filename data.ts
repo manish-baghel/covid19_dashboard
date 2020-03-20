cbf20b517f23e4198d0061191f292f992a3da1ef
Manish baghel <baghelmanish660@gmail.com>
2020-03-20 20:28:00 +0530

    added localStorage for data [update every 1 hour]

--- data.ts
+++ data.ts
@@ -1,40 +1,40 @@
-import csv from 'csvtojson';
+// import csv from 'csvtojson';
 
-const total_cases_uri = "https://covid.ourworldindata.org/data/ecdc/total_cases.csv";
-const total_deaths_uri = "https://covid.ourworldindata.org/data/ecdc/total_deaths.csv";
-const new_confirmed_cases_uri = "https://covid.ourworldindata.org/data/ecdc/new_cases.csv";
-const new_deaths_uri = "https://covid.ourworldindata.org/data/ecdc/new_deaths.csv";
+// const total_cases_uri = "https://covid.ourworldindata.org/data/ecdc/total_cases.csv";
+// const total_deaths_uri = "https://covid.ourworldindata.org/data/ecdc/total_deaths.csv";
+// const new_confirmed_cases_uri = "https://covid.ourworldindata.org/data/ecdc/new_cases.csv";
+// const new_deaths_uri = "https://covid.ourworldindata.org/data/ecdc/new_deaths.csv";
 
-let total_cases = []
+// let total_cases = []
 
-fetch(total_cases_uri)
-	.then(resp => {
-		const reader = resp.body.getReader();
-		return new ReadableStream({
-		    start(controller){
-		      return pump();
-		      function pump(){
-		        return reader.read().then(({ done, value }) => {
-		          // When no more data needs to be consumed, close the stream
-		          if (done) {
-		              controller.close();
-		              return;
-		          }
-		          // Enqueue the next data chunk into our target stream
-		          controller.enqueue(value);
-		          return pump();
-		        });
-		      }
-		    }  
-		  })
-	})
-	.then(stream => new Response(stream))
-	.then(response => response.blob())
-	.then(blob => blob.text())
-	.then(text => {
-		csv()
-			.fromString(text)
-			.then(jsonObj => {
-				console.log(jsonObj);
-			})
-	});
+// fetch(total_cases_uri)
+// 	.then(resp => {
+// 		const reader = resp.body.getReader();
+// 		return new ReadableStream({
+// 		    start(controller){
+// 		      return pump();
+// 		      function pump(){
+// 		        return reader.read().then(({ done, value }) => {
+// 		          // When no more data needs to be consumed, close the stream
+// 		          if (done) {
+// 		              controller.close();
+// 		              return;
+// 		          }
+// 		          // Enqueue the next data chunk into our target stream
+// 		          controller.enqueue(value);
+// 		          return pump();
+// 		        });
+// 		      }
+// 		    }  
+// 		  })
+// 	})
+// 	.then(stream => new Response(stream))
+// 	.then(response => response.blob())
+// 	.then(blob => blob.text())
+// 	.then(text => {
+// 		csv()
+// 			.fromString(text)
+// 			.then(jsonObj => {
+// 				console.log(jsonObj);
+// 			})
+// 	});
--- index.ts
+++ index.ts
@@ -5,6 +5,8 @@ import './data.ts';
 
 import csv from 'csvtojson';
 
+const ls = window.localStorage;
+
 const total_cases_uri = "https://covid.ourworldindata.org/data/ecdc/total_cases.csv";
 const total_deaths_uri = "https://covid.ourworldindata.org/data/ecdc/total_deaths.csv";
 const new_confirmed_cases_uri = "https://covid.ourworldindata.org/data/ecdc/new_cases.csv";
@@ -12,117 +14,140 @@ const new_deaths_uri = "https://covid.ourworldindata.org/data/ecdc/new_deaths.cs
 
 let total_cases = []
 
-Promise.all([
-  fetch(total_cases_uri),
-  fetch(total_deaths_uri),
-  fetch(new_confirmed_cases_uri),
-  fetch(new_deaths_uri)
-]).then(([tcases,tdeaths,ncases,ndeaths]) => {
-    const reader1 = tcases.body.getReader();
-    const reader2 = tdeaths.body.getReader();
-    const reader3 = ncases.body.getReader();
-    const reader4 = ndeaths.body.getReader();
-    return [ 
-      new ReadableStream({
-        start(controller){
-          return pump();
-          function pump(){
-            return reader1.read().then(({ done, value }) => {
-              if (done) {
-                  controller.close();
-                  return;
-              }
-              controller.enqueue(value);
+async function getData(){
+    return Promise.all([
+      fetch(total_cases_uri),
+      fetch(total_deaths_uri),
+      fetch(new_confirmed_cases_uri),
+      fetch(new_deaths_uri)
+    ]).then(([tcases,tdeaths,ncases,ndeaths]) => {
+        const reader1 = tcases.body.getReader();
+        const reader2 = tdeaths.body.getReader();
+        const reader3 = ncases.body.getReader();
+        const reader4 = ndeaths.body.getReader();
+        return [ 
+          new ReadableStream({
+            start(controller){
               return pump();
-            });
-          }
-        }  
-      }),
-      new ReadableStream({
-        start(controller){
-          return pump();
-          function pump(){
-            return reader2.read().then(({ done, value }) => {
-              if (done) {
-                  controller.close();
-                  return;
+              function pump(){
+                return reader1.read().then(({ done, value }) => {
+                  if (done) {
+                      controller.close();
+                      return;
+                  }
+                  controller.enqueue(value);
+                  return pump();
+                });
               }
-              controller.enqueue(value);
+            }  
+          }),
+          new ReadableStream({
+            start(controller){
               return pump();
-            });
-          }
-        }  
-      }),
-      new ReadableStream({
-        start(controller){
-          return pump();
-          function pump(){
-            return reader3.read().then(({ done, value }) => {
-              if (done) {
-                  controller.close();
-                  return;
+              function pump(){
+                return reader2.read().then(({ done, value }) => {
+                  if (done) {
+                      controller.close();
+                      return;
+                  }
+                  controller.enqueue(value);
+                  return pump();
+                });
               }
-              controller.enqueue(value);
+            }  
+          }),
+          new ReadableStream({
+            start(controller){
               return pump();
-            });
-          }
-        }  
-      }),
-      new ReadableStream({
-        start(controller){
-          return pump();
-          function pump(){
-            return reader4.read().then(({ done, value }) => {
-              // When no more data needs to be consumed, close the stream
-              if (done) {
-                  controller.close();
-                  return;
+              function pump(){
+                return reader3.read().then(({ done, value }) => {
+                  if (done) {
+                      controller.close();
+                      return;
+                  }
+                  controller.enqueue(value);
+                  return pump();
+                });
               }
-              // Enqueue the next data chunk into our target stream
-              controller.enqueue(value);
+            }  
+          }),
+          new ReadableStream({
+            start(controller){
               return pump();
-            });
-          }
-        }  
+              function pump(){
+                return reader4.read().then(({ done, value }) => {
+                  // When no more data needs to be consumed, close the stream
+                  if (done) {
+                      controller.close();
+                      return;
+                  }
+                  // Enqueue the next data chunk into our target stream
+                  controller.enqueue(value);
+                  return pump();
+                });
+              }
+            }  
+          })
+        ]
       })
-    ]
-  })
-  .then(streams => [new Response(streams[0]),new Response(streams[1]),new Response(streams[2]),new Response(streams[3])])
-  .then(async response => {
-    let blobs = [await response[0].blob(),await response[1].blob(),await response[2].blob(),await response[3].blob()]
-    return blobs
-  })
-  .then(async blobs => {
-    let texts = [await blobs[0].text(),await blobs[1].text(),await blobs[2].text(),await blobs[3].text()]
-    return texts;
-  })
-  .then(async texts => {
-    let tcases = await csv().fromString(texts[0])
-    let tdeaths = await csv().fromString(texts[1])
-    let ncases = await csv().fromString(texts[2])
-    let ndeaths = await csv().fromString(texts[3])
-    plotGraph(tcases,tdeaths,ncases,ndeaths)
-    buildTable(tcases,tdeaths,ncases,ndeaths)
-    
-  });
+      .then(streams => [new Response(streams[0]),new Response(streams[1]),new Response(streams[2]),new Response(streams[3])])
+      .then(async response => {
+        let blobs = [await response[0].blob(),await response[1].blob(),await response[2].blob(),await response[3].blob()]
+        return blobs
+      })
+      .then(async blobs => {
+        let texts = [await blobs[0].text(),await blobs[1].text(),await blobs[2].text(),await blobs[3].text()]
+        return texts;
+      })
+      .then(async texts => {
+        let tcases = await csv().fromString(texts[0])
+        let tdeaths = await csv().fromString(texts[1])
+        let ncases = await csv().fromString(texts[2])
+        let ndeaths = await csv().fromString(texts[3])
+        // plotGraph(tcases,tdeaths,ncases,ndeaths)
+        // buildTable(tcases,tdeaths,ncases,ndeaths)
+        return [tcases,tdeaths,ncases,ndeaths];
+        
+      });
+}
+
+
 
 // Graphs
-function plotGraph(jsonObj1,jsonObj2,jsonObj3,jsonObj4){
-  var ctx = document.getElementById('myChart')
-  var labels = []
+async function plotGraph(){
   var data1 = []
   var data2 = []
   var data3 = []
   var data4 = []
-  let i = 0;
-  for(i=0;i<jsonObj1.length;i++){
-    // console.log(jsonObj[i])
-      let lb = new Date(jsonObj1[i]["date"]).toDateString().substr(4,6);
+  if(!ls.getItem('tcases')){
+    // let resp = await getData();
+    console.log("fetching data");
+    [data1,data2,data3,data4] = await getData();
+    ls.setItem('updated_at',Date.now());
+    ls.setItem('tcases',JSON.stringify(data1))
+    ls.setItem('tdeaths',JSON.stringify(data2))
+    ls.setItem('ncases',JSON.stringify(data3))
+    ls.setItem('ndeaths',JSON.stringify(data4))
+  }else{
+    data1 = JSON.parse(ls.getItem('tcases'))
+    data2 = JSON.parse(ls.getItem('tdeaths'))
+    data3 = JSON.parse(ls.getItem('ncases'))
+    data4 = JSON.parse(ls.getItem('ndeaths'))
+  }
+  var ctx = document.getElementById('myChart')
+  var labels = []
+  let tcases = [],
+      tdeaths = [],
+      ncases = [],
+      ndeaths = []
+  for(let i=0; i< data1.length; ++i){
+    // console.log(i,data1[i]["date"]);
+      let lb = new Date(data1[i]["date"]).toDateString().substr(4,6);
       labels.push(lb);
-      data1.push(jsonObj1[i]["World"]);
-      data2.push(jsonObj2[i]["World"]);
-      data3.push(jsonObj3[i]["World"]);
-      data4.push(jsonObj4[i]["World"]);
+      tcases.push(data1[i]["World"]);
+      tdeaths.push(data2[i]["World"]);
+      ncases.push(data3[i]["World"]);
+      ndeaths.push(data4[i]["World"]);
   }
   var myChart = new Chart(ctx, 
   {
@@ -131,7 +156,7 @@ function plotGraph(jsonObj1,jsonObj2,jsonObj3,jsonObj4){
       labels: labels,
       datasets: [
         {
-          data: data1,
+          data: tcases,
           lineTension: 0,
           backgroundColor: 'transparent',
           borderColor: '#007bff',
@@ -139,7 +164,7 @@ function plotGraph(jsonObj1,jsonObj2,jsonObj3,jsonObj4){
           label:"Total Infected"
         },
         {
-          data: data2,
+          data: tdeaths,
           lineTension: 0,
           backgroundColor: 'transparent',
           borderColor: '#c8465d',
@@ -147,7 +172,7 @@ function plotGraph(jsonObj1,jsonObj2,jsonObj3,jsonObj4){
           label:"Total Deaths"
         },
         {
-          data: data3,
+          data: ncases,
           lineTension: 0,
           backgroundColor: 'transparent',
           borderColor: '#846bbc',
@@ -155,7 +180,7 @@ function plotGraph(jsonObj1,jsonObj2,jsonObj3,jsonObj4){
           label:"New Confirmed Cases"
         },
         {
-          data: data4,
+          data: ndeaths,
           lineTension: 0,
           backgroundColor: 'transparent',
           borderColor: '#c6a422',
@@ -212,7 +237,7 @@ function buildTable(tcases,tdeaths,ncases,ndeaths){
   }
 
 
-  var table = new Tabulator("#example-table", {
+  var table = new Tabulator("#main-data-table", {
     data:dataArray,           //load row data from array
     layout:"fitColumns",      //fit columns to width of table
     responsiveLayout:"hide",  //hide columns that dont fit on the table
@@ -220,9 +245,10 @@ function buildTable(tcases,tdeaths,ncases,ndeaths){
     addRowPos:"top",          //when adding a new row, add it to the top of the table
     history:true,             //allow undo and redo actions on the table
     pagination:"local",       //paginate the data
-    paginationSize:7,         //allow 7 rows per page of data
-    movableColumns:true,      //allow column order to be changed
-    resizableRows:true,       //allow row order to be changed
+    paginationSize:10,         //allow 7 rows per page of data
+    movableColumns:false,      //allow column order to be changed
+    resizableRows:false,
+    resizableColumns:false,       //allow row order to be changed
     initialSort:[             //set the initial sort order of the data
       {column:"tcases", dir:"dsc"},
     ],
@@ -245,4 +271,14 @@ function buildTable(tcases,tdeaths,ncases,ndeaths){
   'use strict'
 
   feather.replace()
+  if(ls.getItem('updated_at')){
+    let ut = ls.getItem('updated_at');
+    let ct = Date.now();
+    let diff = 60*60*1000;
+    if(ct - parseInt(ut) > diff){
+      console.log("Clearing ls");
+      ls.clear();
+    }
+  }
+  plotGraph()
 }())
