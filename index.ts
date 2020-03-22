a3d0b613239a44a7c8ffd62cade5a7f603c90de1
Manish baghel <baghelmanish660@gmail.com>
2020-03-22 15:16:59 +0530

    dist things

--- index.ts
+++ index.ts
@@ -257,21 +257,16 @@ async function generateWorldMap(){
         geographyConfig: {
             borderColor: '#444',
             highlightBorderWidth: 2
-            // don't change color on mouse hover
             highlightFillColor: function(geo) {
                 return geo['fillColor'] || '#F5F5F5';
             },
-            // only change border
             highlightBorderColor: '#444',
-            // show desired information in tooltip
             popupTemplate: function(geo, data) {
-                // don't show tooltip if country don't present in dataset
                 if (!data) {
                  return ['<div class="hoverinfo">',
                     '<strong>', geo.properties.name, '</strong>',
                     '</div>'].join('');
                 }
-                // tooltip content
                 return ['<div class="hoverinfo">',
                     '<strong>', geo.properties.name, '</strong>',
                     '<br>Count: <strong>', data.numberOfThings, '</strong>',
