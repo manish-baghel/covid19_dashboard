6466b3ee6f7bbfbf274882f528534acfb9f215a0
Manish baghel <baghelmanish660@gmail.com>
2020-03-22 17:06:38 +0530

    gtag build

--- covid19.77de5100.js
+++ covid19.77de5100.js
@@ -106872,7 +106872,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50199" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51609" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
