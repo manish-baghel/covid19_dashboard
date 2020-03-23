7781c81d3d73e1e54342fbe998c33074d7f41fa0
Manish baghel <baghelmanish660@gmail.com>
2020-03-23 23:15:38 +0530

    color scheme build

--- dist/covid19.77de5100.js
+++ dist/covid19.77de5100.js
@@ -106907,7 +106907,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "65122" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60602" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
